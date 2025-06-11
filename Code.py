200ae1c1510695fd00aac7350043507187b374ad
ZenitsuMon <cuccuclone@gmail.com>
2025-06-11 14:39:43 +0700

    Currency Conveter

--- Code.py
+++ Code.py
@@ -1,58 +1,8 @@
-quizs = {
-"question1":{
-	"question": "What is the capital of France",
-	"answer": "Paris"	
-},
-	"question2":{
-		"question": "What is the capital of Portugal",
-		"answer": "Lisbon"	
-},
-	"question3":{
-		"question": "What is the capital of Switzerland",
-		"answer": "Bern"	
-},
-	"question4":{
-		"question": "What is the capital of Germany",
-		"answer": "Berlin"	
-},
-	"question5":{
-		"question": "What is the capital of Italy",
-		"answer": "Rome"	
-},
-	"question6":{
-		"question": "What is the capital of Spain",
-		"answer": "Madrid"	
-},
-	"question7":{
-		"question": "What is the capital of Australia",
-		"answer": "Vienna"	
+def main():
+    print("This is a currency converter")
+    amount_of_dollars = eval(input("How much dollar do you want to convert? "))
+    pounds = convert_to_pounds(amount_of_dollars)
+    print(pounds)
 
-	},
-
-
-}
-
-score = 0
-
-for key, value in quizs.items():
-	print(value["question"])
-	answer = input("Answer ? ")
-	if answer.lower() == value["answer"].lower():
-		print("Correct!")
-		score +=1
-		print ("Your score is: ", str(score))
-		print("")
-		print("")
-		print("")
-
-	else:
-		print("Incorrect! The correct answer is: ", value["answer"])
-		print ("Your score is: ", str(score))
-		print("")
-		print("")
-		print("")
-
-percentage = (score/7)*100
-
-print(f"You got {str(score)} out of 7")
-print(f"Your pecentage is: {str(int(percentage))}%")
+convert_to_pounds = lambda amount_of_dollars: amount_of_dollars*0.82
+main()
