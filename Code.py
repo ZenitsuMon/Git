e169165071e80c3d5c65e37daae7a6102920bbe7
ZenitsuMon <cuccuclone@gmail.com>
2025-06-11 15:15:27 +0700

    Word Dictionary

--- Code.py
+++ Code.py
@@ -1,8 +1,8 @@
-def main():
-    print("This is a currency converter")
-    amount_of_dollars = eval(input("How much dollar do you want to convert? "))
-    pounds = convert_to_pounds(amount_of_dollars)
-    print(pounds)
-
-convert_to_pounds = lambda amount_of_dollars: amount_of_dollars*0.82
-main()
+from PyDictionary import PyDictionary
+dictionary = PyDictionary()
+while True:
+    word = input("Enter a word: ")
+    if word =="":
+        break
+    
+    print(str(list(dictionary.meaning(word))))
